ec87aad475602e2a619326e4ccf3512c573ea425
Essam Eid <essameid614@gmail.com>
2021-09-04 22:26:56 +0200

    refactored , now can have motions

---
+++ src/Motion.ts
@@ -0,0 +1,8 @@
+export default class Motion{
+    public val:number;
+    public axis:String;
+    constructor(val:number,axis:String){
+        this.val = val
+        this.axis = axis
+    }
+}
--- src/Rover.ts
+++ src/Rover.ts
@@ -1,36 +1,53 @@
-export default class Rover{
+import Motion from "./Motion";
 
-    private x:number;
-    private y:number;
-    private facingDirection:String;
-    
-    constructor(x:number,y:number,facingDirection:String){
-        this.x = x;
-        this.y = y;
-        this.facingDirection = facingDirection;
-    }
+export default class Rover {
+  private x: number;
+  private y: number;
+  private facingDirection: String;
 
-    public getPosition():String{
-        return `${this.x} ${this.y} ${this.facingDirection}`
-    }
+  constructor(x: number, y: number, facingDirection: String) {
+    this.x = x;
+    this.y = y;
+    this.facingDirection = facingDirection;
+  }
 
-    public move(movementChar:String):String{
-       this.y = this.y+1
-       return this.getPosition()
-    }
-    public getFacingDirection():String{
-        return this.facingDirection
-    }
-    public getX():number{
-        return this.x
-    }
-    public getY():number{
-        return this.y
-    }
-    public resetRover(){
-        this.x=0
-        this.y=0
-        this.facingDirection = "North"
+  public getPosition(): String {
+    return `${this.x} ${this.y} ${this.facingDirection}`;
+  }
+
+  public move(movementChar: String): String {
+    if (movementChar === "F") {
+      //movement on axis
+      let nextMotion: Motion = this.getMotion();
+      if (nextMotion.axis === "y") {
+        this.y = this.y + nextMotion.val;
+      } else {
+        this.x = this.x + nextMotion.val;
+      }
     }
 
+    return this.getPosition();
+  }
+  public getFacingDirection(): String {
+    return this.facingDirection;
+  }
+  public getX(): number {
+    return this.x;
+  }
+  public getY(): number {
+    return this.y;
+  }
+  public resetRover() {
+    this.x = 0;
+    this.y = 0;
+    this.facingDirection = "North";
+  }
+  private getMotion(): Motion {
+    switch (this.facingDirection) {
+      case "North":
+        return new Motion(1, "y");
+      default:
+        return null;
+    }
+  }
 }
