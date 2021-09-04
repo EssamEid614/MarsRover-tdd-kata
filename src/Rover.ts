8d77b3e7bf2bd789b22c80e29ba80ca1cf5fbab3
Essam Eid <essameid614@gmail.com>
2021-09-04 23:37:36 +0200

    passed all for south

--- src/Rover.ts
+++ src/Rover.ts
@@ -4,7 +4,12 @@ export default class Rover {
   private x: number;
   private y: number;
   private facingDirection: String;
-  private circularArrayOfDirections:String[]=["North","East","South","West"]
+  private circularArrayOfDirections: String[] = [
+    "North",
+    "East",
+    "South",
+    "West",
+  ];
   constructor(x: number, y: number, facingDirection: String) {
     this.x = x;
     this.y = y;
@@ -16,22 +21,26 @@ export default class Rover {
   }
 
   public move(movementChar: String): String {
-    if (movementChar === "F" || movementChar=="B") {
+    if (movementChar === "F" || movementChar == "B") {
       //movement on axis
       let nextMotion: Motion = this.getMotion();
-      if(movementChar==="B"){
-        nextMotion.val = nextMotion.val * -1
+      if (movementChar === "B") {
+        nextMotion.val = nextMotion.val * -1;
       }
       if (nextMotion.axis === "y") {
         this.y = this.y + nextMotion.val;
+      } else {
+        this.x = this.x + nextMotion.val;
       }
+    } else if (movementChar === "L" || movementChar === "R") {
+      const preOrNext: number = movementChar === "L" ? 7 : 9;
+      const currentIndex: number = this.circularArrayOfDirections.indexOf(
+        this.facingDirection
+      );
+      this.facingDirection =
+        this.circularArrayOfDirections[(currentIndex + preOrNext) % 4];
     }
-    else if(movementChar === "L" || movementChar === "R"){
-        const preOrNext:number = movementChar==="L"?7:9
-        const currentIndex:number = this.circularArrayOfDirections.indexOf(this.facingDirection)
-        this.facingDirection=this.circularArrayOfDirections[(currentIndex+preOrNext)%4]
-    }
-    
+
     return this.getPosition();
   }
   public getFacingDirection(): String {
@@ -48,7 +57,7 @@ export default class Rover {
     this.y = 0;
     this.facingDirection = "North";
   }
-  public setRover(x: number, y: number, facingDirection: String){
+  public setRover(x: number, y: number, facingDirection: String) {
     this.x = x;
     this.y = y;
     this.facingDirection = facingDirection;
@@ -57,6 +66,12 @@ export default class Rover {
     switch (this.facingDirection) {
       case "North":
         return new Motion(1, "y");
+      case "South":
+        return new Motion(-1, "y");
+      case "East":
+        return new Motion(1, "x");
+      case "West":
+        return new Motion(-1, "x");
       default:
         return null;
     }
