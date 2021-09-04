6f0277db37e75f78726a731165b2abaaf1fdb1c9
Essam Eid <essameid614@gmail.com>
2021-09-04 22:28:39 +0200

    adding backward functionality back at red

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -17,4 +17,8 @@ describe("Test robot position", () => {
   test("api should respond when asked to move forwards while facing north to add 1 to current Y ", () => {
     return request(app).post("/moveRover").send({movementString:"F"}).expect(200,{roverPosition:"0 1 North"})
   });
+  //Backward while Facing North
+  test("api should respond when asked to move forwards while facing north to add 1 to current Y ", () => {
+    return request(app).post("/moveRover").send({movementString:"B"}).expect(200,{roverPosition:"0 -1 North"})
+  });
 });
